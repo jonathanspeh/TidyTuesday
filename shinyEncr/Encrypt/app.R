19983afcab47db467c6162ed31ee8bd771038049
jonathanspeh <jspeh@gmx.de>
2021-02-09 16:29:30 +0100

    Solved repeated letter problem, found new problem with longer texts

--- shinyEncr/Encrypt/app.R
+++ shinyEncr/Encrypt/app.R
@@ -1,6 +1,6 @@
 # Works so far
 # TODO: Make action buttons useful,
-#       Include "third wheel" or find another solution for the repeated letters problem
+#       figure out what is wrong with long texts (i.e. >27 charcaters?)
 
 
 
@@ -8,58 +8,68 @@ library(shiny)
 library(dplyr)
 
 
-
-
-
 # define custom funnctions
-encr<-function(x){
+encr<-function(x,pcode){
     x<-unlist(strsplit(x,split = ""))
     alp<-c(letters,LETTERS," ","'",".","!","?","Ä","ä","Ö","ö","Ü","ü",as.character(0:9))  
-    set.seed(1)
+    set.seed(pcode)
     num1<-sample(1:length(alp),length(alp),replace = FALSE)
     df1<-data.frame(letter=alp,number=num1)
     set.seed(2)
     num2<-sample(1:length(alp),length(alp),replace = FALSE)
     df2<-data.frame(number=num2,letter=alp)
+    set.seed(3)
+    num3<-sample(1:length(alp),length(alp),replace = FALSE)
+    set.seed(4)
+    num4<-sample(1:length(alp),length(alp),replace = FALSE)
+    df3<-data.frame(inp=num3,outp=num4)
     index<-c()
     for(i in seq_along(x)){
         index[i]<-df1%>%filter(letter==x[i])%>%select(number)
-        df1<-data.frame(letter=alp,number=c(num1[2:length(num1)],num1[1]))
+        df1<-data.frame(letter=alp,
+                        number=c(df1$number[2:length(num1)],df1$number[1]))
     }
     output<-c()
     for(i in seq_along(index)){
         output[i]<-df2%>%filter(number==index[i])%>%select(letter)
-        df2<-data.frame(number=c(num2[2:length(num2)],num2[1]),letter=alp)
+        df2<-data.frame(number=c(df2$number[2:length(num2)-1],df2$number[1]),
+                        letter=alp)
     }
     return(paste0(as.character(unlist(output)),collapse = ""))
 }
 
 
 
-decr<-function(x){
+decr<-function(x,pcode){
     if(is.character(x)==TRUE){
         x<-unlist(strsplit(x,split = ""))
     }
     alp<-c(letters,LETTERS," ","'",".","!","?","Ä","ä","Ö","ö","Ü","ü",as.character(0:9))
-    set.seed(1)
+    set.seed(pcode)
     num1<-sample(1:length(alp),length(alp),replace = FALSE)
     df1<-data.frame(letter=alp,number=num1)
     set.seed(2)
     num2<-sample(1:length(alp),length(alp),replace = FALSE)
     df2<-data.frame(number=num2,letter=alp)
+    set.seed(3)
+    num3<-sample(1:length(alp),length(alp),replace = FALSE)
+    set.seed(4)
+    num4<-sample(1:length(alp),length(alp),replace = FALSE)
+    df3<-data.frame(inp=num3,outp=num4)
     index<-c()
     for(i in seq_along(x)){
         index[i]<-df2%>%filter(letter==x[i])%>%select(number)
-        df2<-data.frame(letter=alp,number=c(num2[2:length(num2)],num2[1]))
+        df2<-data.frame(number=c(df2$number[2:length(num2)-1],df2$number[1]),
+                        letter=alp)
     }
     output<-c()
     for(i in seq_along(index)){
         output[i]<-df1%>%filter(number==index[i])%>%select(letter)
-        df1<-data.frame(number=c(num1[2:length(num1)],num1[1]),letter=alp)
+        df1<-data.frame(letter=alp,
+                        number=c(df1$number[2:length(num1)],df1$number[1]))
     }
     return(paste0(as.character(unlist(output)),collapse = ""))
 }
-
 
 # Define UI 
 ui <- fluidPage(
@@ -72,12 +82,16 @@ ui <- fluidPage(
             textInput("clear",
                       label = "Enter your message in clear text",
                       value = ""),
-            actionButton(inputId = "encrypt",
-                         label = "encrypt"),
             textInput("crypt",
-                      label = "Enter your message in clear text",
+                      label = "Enter your encoded message",
                       value = ""),
-        actionButton("decrypt","decrypt")),
+            numericInput("pin",
+                      label = "Enter your pincode",
+                      value = 1),
+            actionButton(inputId = "encrypt",
+                         label = "encrypt!"),
+            actionButton(inputId = "decrypt",
+                         label = "decrypt!")),
 
         # Show output
         mainPanel(
@@ -91,8 +105,8 @@ ui <- fluidPage(
 
 # Define server logic 
 server <- function(input, output) {
-    output$code<-renderText({encr(input$clear)})
-    output$message<-renderText({decr(input$crypt)})
+    output$code<-renderText({encr(input$clear,input$pin)})
+    output$message<-renderText({decr(input$crypt,input$pin)})
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
--- shinyEncr/Encrypt/reference code.R
+++ shinyEncr/Encrypt/reference code.R
@@ -1,60 +1,71 @@
 # Reference code
 
 
-encr<-function(x){
+encr<-function(x,pcode){
   x<-unlist(strsplit(x,split = ""))
   alp<-c(letters,LETTERS," ","'",".","!","?","Ä","ä","Ö","ö","Ü","ü",as.character(0:9))  
-  set.seed(1)
+  set.seed(pcode)
   num1<-sample(1:length(alp),length(alp),replace = FALSE)
   df1<-data.frame(letter=alp,number=num1)
   set.seed(2)
   num2<-sample(1:length(alp),length(alp),replace = FALSE)
   df2<-data.frame(number=num2,letter=alp)
+  set.seed(3)
+  num3<-sample(1:length(alp),length(alp),replace = FALSE)
+  set.seed(4)
+  num4<-sample(1:length(alp),length(alp),replace = FALSE)
+  df3<-data.frame(inp=num3,outp=num4)
   index<-c()
   for(i in seq_along(x)){
     index[i]<-df1%>%filter(letter==x[i])%>%select(number)
-    df1<-data.frame(letter=alp,number=c(num1[2:length(num1)],num1[1]))
+    df1<-data.frame(letter=alp,
+                    number=c(df1$number[2:length(num1)],df1$number[1]))
   }
   output<-c()
   for(i in seq_along(index)){
     output[i]<-df2%>%filter(number==index[i])%>%select(letter)
-    df2<-data.frame(number=c(num2[1:length(num2)-1],num2[length(num2)]),letter=alp)
+    df2<-data.frame(number=c(df2$number[2:length(num2)-1],df2$number[1]),
+                    letter=alp)
   }
   return(paste0(as.character(unlist(output)),collapse = ""))
 }
 
 
 
-decr<-function(x){
+decr<-function(x,pcode){
   if(is.character(x)==TRUE){
     x<-unlist(strsplit(x,split = ""))
   }
   alp<-c(letters,LETTERS," ","'",".","!","?","Ä","ä","Ö","ö","Ü","ü",as.character(0:9))
-  set.seed(1)
+  set.seed(pcode)
   num1<-sample(1:length(alp),length(alp),replace = FALSE)
   df1<-data.frame(letter=alp,number=num1)
   set.seed(2)
   num2<-sample(1:length(alp),length(alp),replace = FALSE)
   df2<-data.frame(number=num2,letter=alp)
+  set.seed(3)
+  num3<-sample(1:length(alp),length(alp),replace = FALSE)
+  set.seed(4)
+  num4<-sample(1:length(alp),length(alp),replace = FALSE)
+  df3<-data.frame(inp=num3,outp=num4)
   index<-c()
   for(i in seq_along(x)){
     index[i]<-df2%>%filter(letter==x[i])%>%select(number)
-    df2<-data.frame(number=c(num2[1:length(num2)-1],num2[length(num2)]),letter=alp)
+    df2<-data.frame(number=c(df2$number[2:length(num2)-1],df2$number[1]),
+                    letter=alp)
   }
   output<-c()
   for(i in seq_along(index)){
     output[i]<-df1%>%filter(number==index[i])%>%select(letter)
-    df1<-data.frame(number=c(num1[2:length(num1)],num1[1]),letter=alp)
+    df1<-data.frame(letter=alp,
+                    number=c(df1$number[2:length(num1)],df1$number[1]))
   }
   return(paste0(as.character(unlist(output)),collapse = ""))
 }
-  
 
 
-encr("tttttttt")->code
-
-decr(code)
-
+encr("Test",2)->code
+decr(code,1)
 
 
 
@@ -63,5 +74,4 @@ paste0(c("A","B"),collapse = "")
 
 library(shiny)
 runExample("06_tabsets")
-
 
